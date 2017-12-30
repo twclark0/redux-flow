edd9b54f691c6c51b1d1a3e86538eac05fbf92a7
twclark0 <tylerc644@gmail.com>
2017-12-30 00:18:09 -0700

    redux flow setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import logo from './logo.svg'
+import './App.css'
 
-class App extends Component {
+class Counter extends Component {
   render() {
     return (
       <div className="App">
@@ -10,12 +11,19 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button onClick={this.props.decrement}>-</button>
+          <span>{this.props.count}</span>
+        <button onClick={this.props.increment}>+</button>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => ({ count: state.count })
+
+const actions = {
+  increment: () => ({ type: 'INCREMENT' }),
+  decrement: () => ({ type: 'DECREMENT' })
+}
+
+export default connect(mapStateToProps, actions)(Counter)
--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import './index.css'
+import Counter from './App'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const initialState = { count: 0 }
+
+const handleIncrement = (state, action) => ({ count: state.count + 1 })
+
+const handleDecrement = (state, action) => ({ count: state.count - 1 })
+
+const reducer = (state = initialState, action) => {
+  const handlers = {
+      'INCREMENT': handleIncrement,
+      'DECREMENT': handleDecrement
+  }
+  return handlers[action.type] ?
+            handlers[action.type](state, action)
+            : state
+}
+
+const store = createStore(reducer)
+
+const App = () => (
+  <Provider store={store}>
+    <Counter />
+  </Provider>
+)
+
+ReactDOM.render(<App />, document.getElementById('root'))
